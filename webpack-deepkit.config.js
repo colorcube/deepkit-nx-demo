f569b6a76c6d8ae5cbd90c2f68fe7c31a4b548fb
René Fritz <rf@colorcube.de>
2023-01-05 18:24:19 +0100

    fix webpack config to merge getCustomTransformers

--- webpack-deepkit.config.js
+++ webpack-deepkit.config.js
@@ -1,18 +1,28 @@
 const typeCompiler = require('@deepkit/type-compiler');
 
-module.exports = (config, context) => {
-
-  for (const i in config.module.rules) {
-    if (config.module.rules[i].loader && config.module.rules[i].loader.includes('ts-loader')) {
-      // this enables @deepkit/type's type compiler
-      config.module.rules[i].options.getCustomTransformers =
-        (program, getProgram) => ({
-          before: [typeCompiler.transformer],
-          afterDeclarations: [typeCompiler.declarationTransformer],
+module.exports = (config, _context) => {
+    config.module.rules
+        .filter((rule) => rule.loader?.includes('ts-loader'))
+        .forEach((tsRule) => {
+            tsRule.options.getCustomTransformers = addDeepkitTransformer(tsRule.options.getCustomTransformers)
         });
-    }
-  }
 
-  return config;
+    return config
 };
 
+// this enables @deepkit/type's type compiler
+function addDeepkitTransformer(prevGetCustomTransformers) {
+    return (program) => {
+        const customTransformers = { ...(prevGetCustomTransformers ? prevGetCustomTransformers(program) : undefined) };
+        customTransformers.before = [
+            typeCompiler.transformer,
+            ...(customTransformers.before || [])
+        ];
+        customTransformers.afterDeclarations = [
+            typeCompiler.declarationTransformer,
+            ...(customTransformers.afterDeclarations || [])
+        ];
+        // console.log(customTransformers);
+        return customTransformers
+    }
+}
